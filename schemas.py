8f3f3e65b5bc4f18b42523be64535b748b169b2e
wonzz <unbi53@gmail.com>
2026-02-05 14:45:15 +0900

    feat: products schemas ì작성

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, HttpUrl
 
 
 ## Auth
@@ -49,8 +49,23 @@ class WishlistItem(BaseModel):
     category3: Optional[str] = Field(None, description="소분류")
     created_at: datetime = Field(..., description="등록 일시")
 
-class WishlistListResponse(BaseModel):
+# GET /products
+class ProductItem(BaseModel):
+    product_id: str = Field(..., description="네이버 상품 ID")
+    title: str = Field(..., description="상품명")
+    link: HttpUrl = Field(..., description="네이버 쇼핑 링크")
+    lprice: int = Field(..., description="현재 최저가(문자열로 올 수도 있음)")
+    mall_name: Optional[str] = Field(None, description="판매처 이름")
+    image: Optional[HttpUrl] = Field(None, description="상품 이미지 URL")
+    brand: Optional[str] = Field(None, description="브랜드")
+    category1: Optional[str] = Field(None, description="대분류")
+    category2: Optional[str] = Field(None, description="중분류")
+    category3: Optional[str] = Field(None, description="소분류")
+
+
+class ProductsGetResponse(BaseModel):
     result_code: str = Field("SUCCESS", description="결과 코드")
-    total_count: int = Field(..., description="전체 아이템 수")
-    user_id: int = Field(..., description="유저 ID")
-    wishlist_items: List[WishlistItem] = Field(..., description="아이템 목록")
+    total_count: int = Field(..., description="전체 상품 수")
+    page: int = Field(..., description="현재 페이지")
+    size: int = Field(..., description="페이지 당 개수")
+    items: List[ProductItem] = Field(..., description="상품 목록")
